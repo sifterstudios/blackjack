a3befd33c1cc34257acdf2343d80825aab8e40dc
Tormod Liseth <tormod@sifterstudios.no>
2022-11-05 11:41:04 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/test/java/cards/PlayingCardDeck.java
@@ -0,0 +1,21 @@
+package cards;
+
+import Playingcard.PlayingCard;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class PlayingCardDeck {
+    Queue<PlayingCard> cards = new ArrayDeque<>();
+    public void createDeck() {
+
+    }
+
+    public int getDeckSize() {
+        return 52;
+    }
+
+    public PlayingCard getNextCard() {
+        return new PlayingCard();
+    }
+}
